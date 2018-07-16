d0eed74f2c1e86dd758c4e037f967b891eb95459
Mubarak Aminu <mubarakaminu340@gmail.com>
2018-07-16 16:05:22 +0100

    first vue component created

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ title }}</h1>
+    
+    <spice></spice>
+  </div>
+</template>
+
+<script>
+import Spice from './component/spice.vue'
+
+
+export default {
+  components:{
+    'spice':Spice
+
+  },
+  
+  data () {
+    return {
+      title: 'Welcome to Your Spice Vue App'
+    }
+  },
+
+  methods:{
+    greetings:function(){
+      return 'Let Get Started';
+    }
+
+
+  }
+
+}
+
+
+</script>
+
+
+
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/component/Spice.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul>
+    <li v-for="spice in spices">{{ spice }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  
+  data () {
+    return {
+      spices:['Shawarma', 'Snacks', 'Chicken']
+    }
+  }
+
+}
+
+
+</script>
+
